bb8ca01c43afcce379d1942b3b857865aca938a3
Мельничук Полина Максимовна <p_melnichuk12@mail.ru>
2021-06-03 01:08:29 +0300

    переделанное

--- task 1/task 1/task 1.cpp
+++ task 1/task 1/task 1.cpp
@@ -1,141 +1,91 @@
-﻿
-#include <iostream>
-#include <limits>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
-class longlong
+
+class Goods
 {
 private:
-    long s1, s2;
-    unsigned long m1, m2;
-    long first;
-    unsigned long second;
-    long max = numeric_limits<long>::max();
-
-    string tostring()//перевод в строку 
+    string name, date, id;
+    int price, count;
+public:
+    void setName(string name)
     {
-        string Zeros = "";
-        int countOfZeros = (9 - countOfDigits(first)) + (9 - countOfDigits(second)); //Добавляем нули неиспользованным разрядам
-
-        if (first != 0)
-        {
-            for (int i = 0; i < countOfZeros; i++)
-            {
-
-                Zeros += "0";
-            }
-            return to_string(first) + Zeros + to_string(second); //Если старшая часть не равна нулю 
-        }
-        else return to_string(second); //Если старшая часть равна нулю 
+        this->name = name;
     }
-
-    int countOfDigits(int Num) //Считаем к-ство цифр в числе
+    string getName()
     {
-        int count = 0;
-        while (Num >= 1)
-        {
-            Num /= 10;
-            count++;
-        }
-
+        return name;
+    }
+    void setDate(string date)
+    {
+        this->date = date;
+    }
+    string getDate()
+    {
+        return date;
+    }
+    void setPrice(int price)
+    {
+        this->price = price;
+    }
+    int getPrice()
+    {
+        return price;
+    }
+    void setCount(int count)
+    {
+        this->count = count;
+    }
+    int getCount()
+    {
         return count;
     }
-public:
-    longlong(long valueS1, long valueS2, unsigned long valueM1, unsigned long valueM2)//конструктор класса в него передаем все значения
+    void setId(string id)
     {
-        // if (valueS1 > max || valueS2 > max || valueM1 > max || valueM2 > max) throw "Error!";
-      //  if (valueM1<0||valueM2<0) throw "Error!";
-        s1 = valueS1;
-        m1 = valueM1;
-        s2 = valueS2;
-        m2 = valueM2;
-        first = 0;
-        second = 0;
-
-
+        this->id = id;
     }
-    void add()//сложение 
+    string getId()
     {
-        first = s1 + s2;
-        second = m1 + m2;
+        return id;
     }
-    void razn()//вычитание 
+    string toString(int count)
     {
-        first = s1 - s2;
-        second = m2 - m1;
-
+        string temp = name + " " + to_string(count) + " штук(и)" + " будет(ут) стоить " + to_string(count * price) + " руб";
+        return temp;
     }
-    void mult()//умножение
-    {
-        first = s1 * s2;
-        second = m1 * m2;
-
-    }
-    void div()//деление
-    {
-        first = s1 / s2;
-        second = m1 / m2;
-    }
-    void display()//вывод
-    {
-        cout << tostring();
-    }
-
 };
+
 int main()
 {
-    setlocale(LC_ALL, "RUS");
-    long s1, s2;//старшие разряды
-    unsigned long m1, m2;//младшие разряды
-    char vib;//для выбора действия
+    setlocale(LC_ALL, "Russian");
+    Goods pizza;
+    Goods cake;
 
-    cout << "Введите старший разряд первого числа: ";
-    cin >> s1;
+    pizza.setName("Пицца");
+    int priceP;
+    cout << "Введите цену пиццы\n";
+    cin >> priceP;
+    pizza.setPrice(priceP);
+    pizza.setDate("03.03.2021");
+    int countP;
+    cout << "\nВведите количество пиццы\n";
+    cin >> countP;
+    pizza.setCount(countP);
+    pizza.setId("23у165893");
 
-    cout << "Введите младший разряд первого числа: ";
-    cin >> m1;
+    cake.setName("Торт");
+    int priceC;
+    cout << "\nВведите цену торта\n";
+    cin >> priceC;
+    cake.setPrice(priceC);
+    cake.setDate("05.03.2021");
+    int countC;
+    cout << "\nВведите количество тортов\n";
+    cin >> countC;
+    cake.setCount(countC);
+    cake.setId("24у756218");
 
-    cout << "\nВведите старший разряд второго числа: ";
-    cin >> s2;
-
-    cout << "Введите младший разряд второго числа: ";
-    cin >> m2;
-    longlong ls(s1, s2, m1, m2);//вызов обьекта класса и передача данных в класс
-
-f://метка для перемещения в случае ввода неправильной операции
-
-    cout << "Выберите операцию с числами:+,-,/,*: ";
-
-    cin >> vib;//ввод операции
-
-
-    switch (vib)
-    {
-    case('+')://вывод суммы
-        ls.add();
-        cout << "Сумма чисел равна: "; ls.display(); cout << endl;
-        break;
-
-    case('-')://вывод разности
-        ls.razn();
-        cout << "Разность чисел равна: "; ls.display(); cout << endl;
-        break;
-
-    case('*')://произведение чисел 
-        ls.mult();
-        cout << "Произведение чисел равно: "; ls.display(); cout << endl;
-        break;
-
-    case('/')://деление
-        ls.div();
-        cout << " частное чисел равно: "; ls.display(); cout << endl;
-        break;
-
-    default:
-        system("cls");
-        cout << "Введены неверные данные"; cout << endl;
-        goto f;//переходная метка
-    }
-
+    cout << endl << pizza.toString(countP) << endl;
+    cout << cake.toString(countC) << endl;
 }
