be849e7ccbeb8768d95c3ab986acb1b00902a02b
Мельничук Полина Максимовна <p_melnichuk12@mail.ru>
2021-06-10 16:20:33 +0300

    убрала мейн закинула в срр реализацию

--- goods/goods.cpp
+++ goods/goods.cpp
@@ -1,37 +1,121 @@
-﻿#include <iostream>
-#include "Goods.h"
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+
 using namespace std;
+/**
+ * \brief Класс Goods для работы с товарами в магазине
+ */
 
-int main()
+class Goods
 {
-    setlocale(LC_ALL, "Russian");
-    Goods pizza;
-    Goods cake;
+private:
+    /**
+ * \brief поля наименования, даты и индекса, которые используют тип string
+ */
+    string name, date, id;
+    /**
+ * \brief поля цены и общей стоимости, которые инициализированы для дальнейшей работы с конструкторами
+ */
+    double price = 0, count = 0;
+public:
+    /**
+ * \brief конструктор по умолчанию
+ */
+    Goods() = delete;
+    /**
+ * \brief конструктор инициализации
+ */
+    Goods(const double price, const double count);
+    /**
+ * \brief деструктор
+ */
+    ~Goods() = default;
+    /**
+ * \brief метод, устанавливающий значение имени
+ */
+    void setName(const string& name)
+    {
+        this->name = name;
+    }
+    /**
+ * \brief метод, возвращающий имя
+ */
+    string getName() const
+    {
+        return name;
+    }
+    /**
+ * \brief метод, устанавливающий значение даты
+ */
+    void setDate(const string& date)
+    {
+        this->date = date;
+    }
+    /**
+ * \brief метод, возвращающий дату
+ */
+    string getDate() const
+    {
+        return date;
+    }
+    /**
+ * \brief метод, устанавливающий значение цены
+ */
+    void setPrice(double price)
+    {
+        this->price = price;
+    }
+    /**
+ * \brief метод, возвращающий значение цены
+ */
+    int getPrice() const
+    {
+        return price;
+    }
+    /**
+ * \brief метод, возварщающий стоимость всех товаров одного наименования
+ */
+    double getTotalPrice()
+    {
+        return this->price * this->count;
+    }
+    /**
+ * \brief метод, устанавливающий итоговую стоимость
+ */
+    void setCount(double count)
+    {
+        this->count = count;
+    }
+    /**
+ * \brief метод, возвращающий итоговую стоимость
+ */
+    int getCount() const
+    {
+        return count;
+    }
+    /**
+ * \brief метод, устанавливающий индекс товара
+ */
+    void setId(const string& id)
+    {
+        this->id = id;
+    }
+    /**
+ * \brief метод, возвращающий индекс товара
+ */
+    string getId() const
+    {
+        return id;
+    }
+    /**
+ * \brief метод, потокового вывода количества, наименования и полной стоимости товара в строчку
+ */
+    stringstream& operator<< (std::ostream& out, const Goods& goods)
+    {
+        out << goods.getName() << " " << goods.getCount() << " штук(и) будет(ут) стоить " << goods.getTotalPrice() << " руб" << endl;
+    }
+};
 
-    pizza.setName("Пицца");
-    int priceP;
-    cout << "Введите цену пиццы\n";
-    cin >> priceP;
-    pizza.setPrice(priceP);
-    pizza.setDate("03.03.2021");
-    int countP;
-    cout << "\nВведите количество пиццы\n";
-    cin >> countP;
-    pizza.setCount(countP);
-    pizza.setId("23у165893");
 
-    cake.setName("Торт");
-    int priceC;
-    cout << "\nВведите цену торта\n";
-    cin >> priceC;
-    cake.setPrice(priceC);
-    cake.setDate("05.03.2021");
-    int countC;
-    cout << "\nВведите количество тортов\n";
-    cin >> countC;
-    cake.setCount(countC);
-    cake.setId("24у756218");
-
-    pizza.stringstream(); 
-    cake.stringstream();
-}
--- goods/goods.h
+++ goods/goods.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
 using namespace std;
 /**
  * \brief Класс Goods для работы с товарами в магазине
@@ -22,7 +23,7 @@ public:
     /**
  * \brief конструктор по умолчанию
  */
-    Goods() = default;
+    Goods() = delete;
     /**
  * \brief конструктор инициализации
  */
@@ -36,84 +37,72 @@ public:
  */
     void setName(const string& name)
     {
-        this->name = name;
     }
     /**
  * \brief метод, возвращающий имя
  */
-    string getName()
+    string getName() const
     {
-        return name;
     }
     /**
  * \brief метод, устанавливающий значение даты
  */
     void setDate(const string& date)
     {
-        this->date = date;
     }
     /**
  * \brief метод, возвращающий дату
  */
-    string getDate()
+    string getDate() const
     {
-        return date;
     }
     /**
  * \brief метод, устанавливающий значение цены
  */
     void setPrice(double price)
     {
-        this->price = price;
     }
     /**
  * \brief метод, возвращающий значение цены
  */
-    int getPrice()
+    int getPrice() const
     {
-        return price;
     }
     /**
  * \brief метод, возварщающий стоимость всех товаров одного наименования
  */
     double getTotalPrice()
     {
-        return this->price * this->count;
     }
     /**
  * \brief метод, устанавливающий итоговую стоимость
  */
     void setCount(double count)
     {
-        this->count = count;
     }
     /**
  * \brief метод, возвращающий итоговую стоимость
  */
-    int getCount()
+    int getCount() const
     {
-        return count;
     }
     /**
  * \brief метод, устанавливающий индекс товара
  */
     void setId(const string& id)
     {
-        this->id = id;
     }
     /**
  * \brief метод, возвращающий индекс товара
  */
-    string getId()
+    string getId() const
     {
-        return id;
     }
     /**
  * \brief метод, потокового вывода количества, наименования и полной стоимости товара в строчку
  */
-    void stringstream()
+     stringstream& operator<< (std::ostream & out, const Goods& goods)
     {
-        cout << name << " " << count << " штук(и) будет(ут) стоить " << getTotalPrice() << " руб" << endl; 
     }
 };
 
